9b8c8e37c8aeba4787fced40f597a1b25723ba3f
Yasin Hakim <ibnkhaqim@gmail.com>
2026-07-17 14:47:03 +0700

    add: Piece Struct and NewBoard function

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("Chess server")
+type PieceType int
+
+const (
+	NoPieceType PieceType = iota
+	King
+	Queen
+	Rook
+	Bishop
+	Knight
+	Pawn
+)
+
+type PieceColor int
+
+const (
+	NoPieceColor PieceColor = iota
+	Black
+	White
+)
+
+type Piece struct {
+	pieceType  PieceType
+	pieceColor PieceColor
+}
+
+func NewBoard() [8][8]Piece {
+	Board := [8][8]Piece{}
+	return Board
+}
+
+func main() {
+	Board := NewBoard()
+	fmt.Println(Board)
 }
