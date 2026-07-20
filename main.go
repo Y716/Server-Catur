26bd645f653317bcc2bebf289c271743c1257334
Yasin Hakim <ibnkhaqim@gmail.com>
2026-07-20 19:57:21 +0700

    add: MovePiece Function

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type PieceType int
@@ -22,6 +26,19 @@ const (
 	NoPieceColor PieceColor = iota
 	Black
 	White
+)
+
+type File int
+
+const (
+	a File = iota
+	b
+	c
+	d
+	e
+	f
+	g
+	h
 )
 
 type Piece struct {
@@ -73,7 +90,47 @@ func PrintBoard(Board [8][8]Piece) {
 	}
 }
 
+func alphaToNum(s string) int {
+	// Standardize to uppercase to easily handle mixed input
+	s = strings.ToUpper(s)
+
+	result := 0
+	for i := 0; i < len(s); i++ {
+		// Convert character to 1-26 value
+		charValue := int(s[i] - 'A' + 1)
+
+		// Handle multi-letter lists (like Excel columns: A=1, Z=26, AA=27)
+		result = result*26 + charValue
+	}
+	return result
+}
+
+func boardRepToCompRep(number int) int {
+	// Change board representation to array representation: y = -x + 8
+	return number*(-1) + 8
+}
+
+func MovePiece(from string, to string, Board [8][8]Piece) {
+	fromFile := alphaToNum(string(from[0])) - 1
+	fromRank, _ := strconv.Atoi(string(from[1]))
+	toFile := alphaToNum(string(to[0])) - 1
+	toRank, _ := strconv.Atoi(string(to[1]))
+
+	movedPiece := Board[boardRepToCompRep(fromRank)][fromFile]
+	Board[boardRepToCompRep(fromRank)][fromFile] = Piece{NoPieceType, NoPieceColor}
+	Board[boardRepToCompRep(toRank)][toFile] = movedPiece
+
+	PrintBoard(Board)
+}
+
 func main() {
 	Board := NewBoard()
-	PrintBoard(Board)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Your move: ")
+	if scanner.Scan() {
+		input := scanner.Text()
+		notations := strings.Split(input, " ")
+		MovePiece(notations[0], notations[1], Board)
+		PrintBoard(Board)
+	}
 }
