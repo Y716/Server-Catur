a08fd769a136f723d42a0913c2aa00dd8a036438
yasin <yasin@MBP-ITDEV-YASIN.local>
2026-08-21 13:50:54 +0700

    Add: Broadcast function and websocket

--- server/server.go
+++ server/server.go
@@ -9,13 +9,9 @@ import (
 type Response struct{
 	Status string `json:"status"`}
 
-
-
-
-
 func ConnectServer(){
 	http.HandleFunc("/health", HealthHandler)
-	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/broadcast", broadcast)
 	fmt.Println("Server starting on 8080...")
 	http.ListenAndServe(":8080", nil)
 }
