f471d2acf2da8a9337d9367f200e00bd989de047
Y716 <ibnkhaqim@gmail.com>
2026-08-21 15:54:07 +0700

    Add: Room to connect two players, doesn't let a third player

--- server/websocket.go
+++ server/websocket.go
@@ -2,47 +2,76 @@ package server
 
 import (
 	"log"
-	"github.com/gorilla/websocket"
 	"net/http"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
-	
+
 var upgrader = websocket.Upgrader{}
-type SafeConnections struct{
-	connections []*websocket.Conn
+// type SafeConnections struct{
+// 	connections []*websocket.Conn
+// 	mu sync.Mutex
+// }
+
+type Room struct{
+	Player1 *websocket.Conn
+	Player2 *websocket.Conn
 	mu sync.Mutex
 }
 
-var safeConnect = &SafeConnections{}
+var safeRoom = &Room{}
 
 func broadcast(w http.ResponseWriter, r *http.Request){
-
 	conn, err := upgrader.Upgrade(w,r, nil)
 	if err != nil{
 		log.Print("Upgrade:", err)
 		return
 	}
-	safeConnect.mu.Lock()
-	safeConnect.connections = append(safeConnect.connections, conn)
-	safeConnect.mu.Unlock()
 	defer conn.Close()
-	
+	safeRoom.mu.Lock()
+	if safeRoom.Player1 != nil && safeRoom.Player2 != nil{
+		conn.WriteMessage(websocket.TextMessage, []byte( "Room is full. Please come again later!" ))	
+		safeRoom.mu.Unlock()
+		return
+	}else{
+		if safeRoom.Player1 == nil{
+			safeRoom.Player1 = conn
+		}else{
+			safeRoom.Player2 = conn
+		}
+	}
+	safeRoom.mu.Unlock()
+
 	for {
 		mt, message, err := conn.ReadMessage()
 		if err != nil{
 			log.Println("read:", err)
 			break
 		}
-		log.Printf("recv: %s", message)
-		safeConnect.mu.Lock()
-		for _, c := range safeConnect.connections {
-			err = c.WriteMessage(mt, message)
-			if err != nil {
-				log.Println("write:", err)
-				break
-			}
+		err = safeRoom.BroadcastMessage(mt, message)
+		if err != nil{
+			log.Println("write:", err)
 		}
-		safeConnect.mu.Unlock()
+		log.Printf("recv: %s", message)
 	}
 
 }
+
+func (room *Room) BroadcastMessage(mt int, message []byte) error{
+	room.mu.Lock()
+	defer room.mu.Unlock()
+
+	err := room.Player1.WriteMessage(mt, message)
+	if err != nil {
+		return err
+	}
+
+	err = room.Player2.WriteMessage(mt, message)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
